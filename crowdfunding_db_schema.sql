3b11aff69348ee16283141a88420a3df456f4545
cdhardwick57@gmail.com <cdhardwick57@gmail.com>
2022-10-18 20:54:10 -0500

    adding png and sql file

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,15 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "backer" (
+    "backer_id" varchar(10)   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_backer" PRIMARY KEY (
+        "backer_id"
+     )
+);
+
